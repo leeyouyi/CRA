6590493bd8ca2ed751d03c097a0da404d8c71e93
tom <a0986599501@gmail.com>
2023-12-12 08:39:02 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useMutation, useQuery } from "@tanstack/react-query";
+// "proxy": "https://10.88.53.72/dpmswebapi"
 
-function App() {
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  proxy: {
+    host: "localhost",
+    port: 3000,
+  },
+});
+interface login {
+  UserName: string;
+  Paccword: string;
+  CakeTake?: boolean;
+  RememberMe: boolean;
+}
+const baseUrl = "/Take";
+const login = (params: login): Promise<unknown[]> =>
+  instance.post(`${baseUrl}/login`, params).then((response) => response.data);
+
+// const baseUrl = "/zh-tw";
+// const fetchAttractions = (): Promise<unknown[]> =>
+//   axios.get(`${baseUrl}/Attractions/All`).then((response) => response.data);
+
+const App = (): JSX.Element => {
+  console.log(process.env);
+  const mutation = useMutation({
+    mutationFn: login,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          mutation.mutate({
+            UserName: "6735",
+            Paccword: "6735",
+            RememberMe: false,
+            CakeTake: true,
+          });
+        }}
+      >
+        login
+      </button>
     </div>
   );
-}
+};
 
 export default App;
